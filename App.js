ae999d35bbcc1897c7ded19c0c10e52e841fe6d4
sayihhamza <esayihhamza@gmail.com>
2022-09-10 20:16:43 +0100

    stations showed

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
+import GetLocation from 'react-native-get-location';
+import Stations from './Stations.js';
+import {LogBox} from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 MapboxGL.setAccessToken(
   'pk.eyJ1Ijoic2F5aWhoYW16YSIsImEiOiJjbDZ3azdpNmUwankwM2RxbDc1b3Y0bGppIn0.-VJisKK9RbocLKMq52oC6Q',
@@ -23,19 +28,49 @@ const styles = StyleSheet.create({
   },
 });
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.page}>
-        <View style={styles.container}>
-          <MapboxGL.MapView style={styles.map}>
-            <MapboxGL.Camera
-              centerCoordinate={[-4.9889563, 34.0121715]}
-              zoomLevel={11}
-            />
-          </MapboxGL.MapView>
-        </View>
+export default function App() {
+  const [currentPosition, setCurrentPosition] = useState([
+    -4.9889563, 34.0121715,
+  ]);
+
+  useEffect(() => {
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 15000,
+    })
+      .then(location => {
+        console.log(location);
+        setCurrentPosition([location.longitude, location.latitude]);
+      })
+      .catch(error => {
+        const {code, message} = error;
+        console.warn(code, message);
+      });
+  });
+  return (
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <MapboxGL.MapView
+          style={styles.map}
+          onPress={event => {
+            setCurrentPosition(event.geometry.coordinates);
+          }}>
+          <MapboxGL.UserLocation />
+
+          <MapboxGL.Camera centerCoordinate={currentPosition} zoomLevel={15} />
+
+          <MapboxGL.PointAnnotation coordinate={[-4.9889563, 34.0121715]} />
+
+          {Stations?.map((station, index) => (
+            <MapboxGL.PointAnnotation
+              id={station.address}
+              key={index}
+              coordinate={[station.location[1], station.location[0]]}>
+              <MapboxGL.Callout title={station.address} />
+            </MapboxGL.PointAnnotation>
+          ))}
+        </MapboxGL.MapView>
       </View>
-    );
-  }
+    </View>
+  );
 }
---
+++ Stations.js
@@ -0,0 +1,58 @@
+/* eslint-disable prettier/prettier */
+const Stations = [
+  {address: 'G96Q+CR2, Casablanca', location: [33.511017, -7.6126784]},
+  {
+    address: 'H9RH+883, Bd Rachidi, Casablanca 20250',
+    location: [33.5920221, -7.6266354],
+  },
+  {
+    address: 'H9P6+JMX, Bd Al Massira Al Khadra, Casablanca 20250',
+    location: [33.5868012, -7.640128],
+  },
+  {
+    address: 'F9MG+HFX, Bouskoura',
+    location: [33.483992, -7.6259596],
+  },
+  {
+    address: '17 R. Ibnou Khalikane, Casablanca 20250',
+    location: [33.582074, -7.6270767],
+  },
+  {address: 'FF3H+PH3, Mediouna 10110', location: [33.4542632, -7.5232796]},
+  {
+    address: 'JXCX+R5H, Rue Harroun Errachid،, Marrakesh 40000',
+    location: [31.622067, -8.0042096],
+  },
+  {address: 'W64C+8QQ, Skhirat, Témara', location: [33.9058419, -6.7802096]},
+  {address: 'M2H2+PPM, Marrakech', location: [31.679342, -8.0003971]},
+  {address: '6G24+MFG, El Jadida', location: [33.201692, -8.4959909]},
+  {
+    address: 'X599+5H3, Avenue Mohammed VI, Av. Mohamed VI, Rabat 10100',
+    location: [33.9723249, -6.8512492],
+  },
+  {address: 'W35W+W6C, R403, Khouribga', location: [32.9083242, -6.9081736]},
+  {address: 'X49P+H8H, Rabat', location: [33.9689419, -6.8663346]},
+  {
+    address: '6 Rue Omar Al Jadidi, Rabat 10050',
+    location: [34.011573, -6.8549293],
+  },
+  {
+    address: 'Errabii 03, Settat 26000',
+    location: [33.0099418, -7.6366417],
+  },
+  {address: '6CH9+CHC, Kénitra', location: [34.2285669, -6.5833034]},
+  {
+    address: '593, Av. La Marche Verte Hay El Jadid، Tiflet 15400',
+    location: [33.8888215, -6.3219554],
+  },
+  {
+    address: 'Rte de Oued Zem, Khouribga 25000',
+    location: [32.9018897, -6.8389407],
+  },
+  {
+    address: 'A3 Autoroute Casa-Marrakech,A7, Ben Guerir 43150',
+    location: [31.6887287, -8.2718735],
+  },
+  {address: 'X2WJ+P2, Fes', location: [33.9980365, -4.9706468]},
+];
+
+export default Stations;
